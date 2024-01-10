0db97317209770b101aea0b5b101231f4a0b0e02
sbrown7861 <skyler.brown15@yahoo.com>
2024-01-09 16:08:35 -0800

    dynamic active nav links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-// Nav toggle
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const linkList = document.querySelectorAll(".nav-links li");
+const currentPage = location.pathname.split("/")[1];
 
+// Nav toggle
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
-  links.forEach((link) => {
-    link.classList.toggle("fade");
+  linkListItem.forEach((item) => {
+    item.classList.toggle("fade");
   });
 
   hamburger.classList.toggle("toggle");
+});
+
+// Active page
+linkList.forEach((item) => {
+  let link = item.firstChild;
+  link.getAttribute("href") === "#" ? link.classList.add("active") : "";
 });
